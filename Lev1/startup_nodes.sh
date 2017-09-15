ee426814e92f02c778412cd8478cd70db4585ef1
Fredrik Hansson <fredrik.hansson@regionuppsala.se>
2017-09-15 16:01:38 +0200

    Uppdaterar med filer från bs-ap-20

--- Lev1/startup_nodes.sh
+++ Lev1/startup_nodes.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-# Loopar igenom servrarna i filen /etc/gridhosts.
-i=1
-for hst in `cat /etc/gridhosts`
+local_host="$(hostname)"
+# Tar bort allt efter första punkten
+local_host="${local_host%%.*}"
+
+# Loopar igenom servrarna i filen /etc/gridhosts. Kör inte kommandot för local host.
+for hst in `tac /etc/gridhosts`
 do
 	echo 
-	ssh $hst "hostname; echo \ /opt/sas/config/Lev1/sas.servers start; /opt/sas/config/Lev1/sas.servers start;"
+	if [ "$local_host" != "$hst" ]
+	then
+		ssh $hst "hostname; echo \ /opt/sas/config/Lev1/sas.servers start; /opt/sas/config/Lev1/sas.servers start;"
+		echo 
+		ssh $hst "hostname; echo \ /opt/sas/sashome/SASDeploymentAgent/9.4/agent.sh start; /opt/sas/sashome/SASDeploymentAgent/9.4/agent.sh start;"
+	else
+		echo "=========================================================================================================================="
+		echo "Command was not run for $hst (local host). Local host should have been started manually before running this script.";
+		echo "=========================================================================================================================="
+	fi
 	echo 
-	ssh $hst "hostname; echo \ /opt/sas/sashome/SASDeploymentAgent/9.4/agent.sh start; /opt/sas/sashome/SASDeploymentAgent/9.4/agent.sh start;"
-	echo
-	i=$((i + 1))
 done 
--- Lev1/status_nodes.sh
+++ Lev1/status_nodes.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-
-# Loopar igenom servrarna i filen /etc/gridhosts.
+echo "================================================================================================"
 echo 
-echo "======== Notera att det här rogrammet inte kontrollerar status på SASDeploymentAgent eftersom den inte verkar ha någon status-funktionalitet"
+echo "Loopar igenom servrarna i filen /etc/gridhosts och anropar kommandot"
+echo "/opt/sas/config/Lev1/sas.servers status"
 echo 
-i=1
+echo "Notera att det här rogrammet inte kontrollerar status på SASDeploymentAgent"
+echo "eftersom den inte verkar ha någon status-funktionalitet"
+echo 
+echo "================================================================================================"
 for hst in `tac /etc/gridhosts`
 do
 	echo 
-	ssh $hst "hostname; echo \ /opt/sas/config/Lev1/sas.servers status; /opt/sas/config/Lev1/sas.servers status;"
+	ssh $hst "hostname; /opt/sas/config/Lev1/sas.servers status;"
 	echo
-	i=$((i + 1))
 done 
