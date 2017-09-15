1c8f2b7c155b911be0874d65b60aa9c3bfbc39bd
Fredrik Hansson <fredrik.hansson@regionuppsala.se>
2017-09-15 10:36:51 +0200

    Utesluter huvudnoden ur start- och stop-skripten

--- Lev1/shutdown_nodes.sh
+++ Lev1/shutdown_nodes.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
+local_host="$(hostname)"
+# Tar bort allt efter första punkten
+local_host="${local_host%%.*}"
+
+
 # Loopar igenom servrarna i filen /etc/gridhosts. Filen läses nerifrån och upp för att huvudnoden ska stängas av sist!
-i=1
 for hst in `tac /etc/gridhosts`
 do
-	echo 
-	ssh $hst "hostname; echo \ /opt/sas/config/Lev1/sas.servers stop; /opt/sas/config/Lev1/sas.servers stop;"
-	echo 
-	ssh $hst "hostname; echo \ /opt/sas/sashome/SASDeploymentAgent/9.4/agent.sh stop; /opt/sas/sashome/SASDeploymentAgent/9.4/agent.sh stop;"
 	echo
-	i=$((i + 1))
+	if [ "$local_host" != "$hst" ]
+	then
+		ssh $hst "hostname; echo \ /opt/sas/config/Lev1/sas.servers stop; /opt/sas/config/Lev1/sas.servers stop;"
+		echo 
+		ssh $hst "hostname; echo \ /opt/sas/sashome/SASDeploymentAgent/9.4/agent.sh stop; /opt/sas/sashome/SASDeploymentAgent/9.4/agent.sh stop;"
+	else
+		echo "=========================================================================================================================="
+		echo "Command was not run for $hst (local host). Local host should be shut down manually after running this script.";
+		echo "=========================================================================================================================="
+	fi
+	echo 
 done 
+
+
