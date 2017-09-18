e3f1ecf2d216a7a7353306ec735bd72c6671dcca
Fredrik Hansson <fredrik.hansson@regionuppsala.se>
2017-09-18 11:49:12 +0200

    Skapar en katalog

--- Lev1/SASApp_VA/BatchServer/sasbatch.sh
+++ Lev1/SASApp_VA/BatchServer/sasbatch.sh
@@ -25,6 +25,9 @@ CONFIGDIR="$APPSERVER_ROOT"/'BatchServer'
 SASCFGPATH="/opt/sas/config/Lev1/SASApp_VA/sasv9.cfg, /opt/sas/config/Lev1/SASApp_VA/sasv9_usermods.cfg, /opt/sas/config/Lev1/SASApp_VA/BatchServer/sasv9.cfg, /opt/sas/config/Lev1/SASApp_VA/BatchServer/sasv9_usermods.cfg"
 export SASCFGPATH
 
+# Skapa katalog för returkod, om den inte redan finns
+cd /saswork/batchrun || mkdir /saswork/batchrun
+
 "$SAS_COMMAND" -noxcmd -lrecl 32767 "$@" "${USERMODS_OPTIONS[@]}"
 # To allow SAS warnings (rc==1) to be treated as okay, remove the "exec" on the line above and uncomment the lines below.
 rc=$?
