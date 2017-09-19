96b790dc2f22f24dd0641f4dfd832aff7e0dc648
Fredrik Hansson <fredrik.hansson@regionuppsala.se>
2017-09-19 13:25:29 +0200

    Lägger till några config-filer från MetadataServern

---
+++ Lev1/SASMeta/MetadataServer/MetadataServer_usermods.sh
@@ -0,0 +1,9 @@
+#!/bin/sh -p
+#
+# MetadataServer_usermods.sh
+#
+# This script extends MetadataServer.sh.  Add local environment variables
+# to this file so they will be preserved.
+#
+
+USERMODS_OPTIONS=
