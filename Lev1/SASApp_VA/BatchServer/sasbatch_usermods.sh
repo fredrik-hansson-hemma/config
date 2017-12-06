ec9684c6773fec2a724394780f4691027c830718
Fredrik Hansson <fredrik.hansson@regionuppsala.se>
2017-12-06 13:31:28 +0100

    sasbatch_usermods.sh: Tar bort några mellanslag samt byter till Linux-format

--- Lev1/SASApp_VA/BatchServer/sasbatch_usermods.sh
+++ Lev1/SASApp_VA/BatchServer/sasbatch_usermods.sh
@@ -9,10 +9,10 @@
 set -A USERMODS_OPTIONS   # initialize empty list
 # build up list by un-commenting (and adding) the lines you need (one line per token)
 # then replace the <argument> with the values you want for each token on the command line
-USERMODS_OPTIONS[${#USERMODS_OPTIONS[*]}]="-errorabend"  
-USERMODS_OPTIONS[${#USERMODS_OPTIONS[*]}]="-xcmd"  
-#USERMODS_OPTIONS[${#USERMODS_OPTIONS[*]}]="<argument>"  
-#USERMODS_OPTIONS[${#USERMODS_OPTIONS[*]}]="<argument>"  
-#USERMODS_OPTIONS[${#USERMODS_OPTIONS[*]}]="<argument>"  
-#USERMODS_OPTIONS[${#USERMODS_OPTIONS[*]}]="<argument>"  
-#USERMODS_OPTIONS[${#USERMODS_OPTIONS[*]}]="<argument>"  
+USERMODS_OPTIONS[${#USERMODS_OPTIONS[*]}]="-errorabend"
+USERMODS_OPTIONS[${#USERMODS_OPTIONS[*]}]="-xcmd"
+#USERMODS_OPTIONS[${#USERMODS_OPTIONS[*]}]="<argument>"
+#USERMODS_OPTIONS[${#USERMODS_OPTIONS[*]}]="<argument>"
+#USERMODS_OPTIONS[${#USERMODS_OPTIONS[*]}]="<argument>"
+#USERMODS_OPTIONS[${#USERMODS_OPTIONS[*]}]="<argument>"
+#USERMODS_OPTIONS[${#USERMODS_OPTIONS[*]}]="<argument>"
